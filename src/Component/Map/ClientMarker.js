ecaac4d1bcbf9ffa52b3bb29a16c22609179d12f
C.Victor <c>
2020-05-10 23:56:41 +0800

    [New] Add map marker filter

--- src/Component/Map/ClientMarker.js
+++ src/Component/Map/ClientMarker.js
@@ -1,41 +1,155 @@
 import React from 'react';
 
 import IconButton from '@material-ui/core/IconButton';
-import {Add, LocalHospital, Healing, Close} from '@material-ui/icons';
- function ClientMark({generatedBy, clientStatus}){
-    if (generatedBy == "status"){
-        
-      if(clientStatus == "discharged"){
-        return(
+import {Add, LocalHospital, Healing, Close, FlightLand, People, PersonPin} from '@material-ui/icons';
+
+const confirmedColour = '#1c7800'
+const possiblyColour = '#545454'
+
+//Markers for Classification
+const importedMarker = () => {
+	return (
+        <IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+          <FlightLand style={{fontSize: 16}}/>
+        </IconButton>
+	)
+}
+const localCloseContactMarker = () => {
+	return (
+        <IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+          <People style={{fontSize: 16}}/>
+        </IconButton>
+	)
+}
+const localPossiblyMarker = () => {
+	return (
+		<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+		  <PersonPin style={{fontSize: 16}}/>
+		</IconButton>
+	)
+}
+const localPossiblyCloseContactMarker = () => {
+	return (
+		<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+		  <People style={{fontSize: 16}}/>
+		</IconButton>
+	)
+	
+}
+const localMarker = () => {
+	return (
+		<IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+		  <People style={{fontSize: 16}}/>
+		</IconButton>
+	)
+}
+//Markers for Status
+
+const dischargedMarker = () => {
+	return (
         <IconButton style={{ padding: '1px',backgroundColor: '#00ff00'}}>
-          <Healing style={{fontSize: 14}}/>
+          <Healing style={{fontSize: 16}}/>
+        </IconButton>
+	)
+}
+const hospitalisedMarker = () => {
+	return (
+        <IconButton style={{ padding: '1px',backgroundColor: '#ff7575'}}>
+          <LocalHospital style={{fontSize: 16}}/>
+        </IconButton>
+	)
+}
+const deceasedMarker = () => {
+	return (
+		<IconButton style={{ padding: '1px',backgroundColor: '#545454'}}>
+		  <Close style={{fontSize: 16}}/>
+		</IconButton>
+	)
+}
+const criticalMarker = () => {
+	return (
+		<IconButton style={{ padding: '1px',backgroundColor: '#ff0000'}}>
+		  <LocalHospital style={{fontSize: 16}}/>
+		</IconButton>
+	)
+}
+
+ function ClientMarkByClassification({clientClassification}){
+	 console.log("clientClassification: "+clientClassification)
+	 if (clientClassification!=null){
+      if(clientClassification == "imported"){
+        return(
+        <IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+          <FlightLand style={{fontSize: 16}}/>
         </IconButton>
         )
       }
-      else if (clientStatus == "hospitalised" || clientStatus == "hospitalised_again" || clientStatus == "serious" || clientStatus == "cricital" ){
+	  else if (clientClassification == "local_close_contact"){
         return(
-        <IconButton style={{ padding: '1px',backgroundColor: '#ff7575'}}>
-          <LocalHospital style={{fontSize: 14}}/>
+        <IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+          <People style={{fontSize: 16}}/>
         </IconButton>
+        )
+      }
+      else if (clientClassification == "local_possibly"){
+        return (
+		<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+		  <PersonPin style={{fontSize: 16}}/>
+		</IconButton>
+        )
+      }
+      else if (clientClassification == "local_possibly_close_contact"){
+        return (
+		<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+		  <People style={{fontSize: 16}}/>
+		</IconButton>
+        )
+      }
+      else if (clientClassification == "local"){
+        return (
+		<IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+		  <People style={{fontSize: 16}}/>
+		</IconButton>
+        )
+      }
+	 return (
+	 
+        null
+	 )
+	 }
+	return null
+ }
+ function ClientMarkByStatus({clientStatus}){
+      if(clientStatus == "discharged"){
+        return(
+		dischargedMarker()
+        )
+      }
+      else if (clientStatus == "hospitalised" || clientStatus == "hospitalised_again" ){
+        return(
+        hospitalisedMarker()
         )
       }
       else if (clientStatus == "deceased"){
         return (
-            <IconButton style={{ padding: '1px',backgroundColor: '#545454'}}>
-              <Close style={{fontSize: 14}}/>
-            </IconButton>
+            deceasedMarker()
         )
       }
       else if (clientStatus == "critical" || clientStatus == "serious"){
         return (
-            <IconButton style={{ padding: '1px',backgroundColor: '#ff0000'}}>
-              <LocalHospital style={{fontSize: 14}}/>
-            </IconButton>
+            criticalMarker()
         )
       }
+ }
+ function ClientMarker({generatedBy, clientStatus, clientClassification}){
+    if (generatedBy == 0){
+        return <ClientMarkByStatus clientStatus ={ clientStatus}/>
     }
+	else if (generatedBy == 1){
+		return <ClientMarkByClassification clientClassification={clientClassification}/>
+	}
     return(
       null
     )
   }
-  export default ClientMark
+  export default ClientMarker
--- src/Component/Map/Map.js
+++ src/Component/Map/Map.js
@@ -4,9 +4,15 @@ import GoogleMapReact from 'google-map-react';
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
-import Menu from '@material-ui/core/Menu';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import {Add, LocalHospital, Healing, Close, FlightLand, People, PersonPin} from '@material-ui/icons';
 
 import ClientMarker from './ClientMarker'
 
@@ -14,12 +20,22 @@ import CaseLocationJson from '../../Config/CaseLocation.json';
 import CasesDetailsJson from '../../Config/CasesDetails.json';
 import './Map.css';
 
+const options = [
+		'Client Status',
+		'Case Classification',
+		'Hide sensitive notification content',
+		'Hide all notification content',
+	]
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       CasesDetails: [],
-      CasesLocationDetails: [] 
+      CasesLocationDetails: [],
+	  filterType: "status",
+	  anchorEl: null,
+	  selectedIndex: 0
+	  
     };
   }
   componentWillMount(){
@@ -41,37 +57,26 @@ class Map extends Component {
     },
     zoom: 10
   };
-  
+	handleClickListItem = (event) => {
+		this.setState({anchorEl: event.currentTarget})
+	};
+	handleMenuItemClick = (event, index) => {
+		this.setState({anchorEl: null, selectedIndex: index})
+	};
+
+	handleClose = () => {
+		this.setState({anchorEl: null})
+	};
   toggleMarker(case_no){
     console.log("Marker "+case_no+" is pressed")
   }
   AnyReactComponent = ({ text, data, clientDetail }) => {
+	const {filterType, selectedIndex} = this.state
     var markerColour='#ffffff';
     if (clientDetail!=null){
-      var clientStatus = clientDetail.status
 
-      if(clientStatus == "discharged"){
-        markerColour = "#00ff00"
-      }
-      else if (clientStatus == "hospitalised" || clientStatus == "hospitalised_again" || clientStatus == "serious" || clientStatus == "cricital" ){
-        markerColour = "#ff7575"
-      }
-      else if (clientStatus == "deceased"){
-        markerColour = "#545454"
-      }
-      else if (clientStatus == "critical"){
-        markerColour = "#ff0000"
-      }
       //console.log(data.case_no+": "+ clientStatus+", "+markerColour)
-      if (clientStatus!=null){
-        var titleText = data.case_no+"</br>"
-        titleText+="Confirmation Date: "+clientDetail.confirmation_date+"\n"
-        titleText+="Gender: "+clientDetail.gender+"\n"
-        titleText+="Age: "+clientDetail.age+"\n"
-        titleText+="Hospital: "+clientDetail.hospital_en+"\n"
-        titleText+="Status: "+clientDetail.status_en+"\n"
-        titleText+="Client Type: "+clientDetail.type_en+"\n"
-        titleText+="Classification: "+clientDetail.classification_en+"\n"
+      if (clientDetail.status!=null){
       return(
         <div>
           <Tooltip 
@@ -116,13 +121,111 @@ class Map extends Component {
               border: '1px solid #dadde9',
             }}
           >
-            <ClientMarker generatedBy="status" clientStatus = {clientDetail.status}/>
+		  
+			<ClientMarker generatedBy={selectedIndex} clientStatus = {clientDetail.status} clientClassification = {clientDetail.classification}/>
           </Tooltip>
         </div>
       )
       }
     }
     return null;
+  }
+  MapKey = ({selectedIndex}) => {
+	if (selectedIndex == 0){
+		return(
+			<table>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: '#00ff00'}}>
+					  <Healing style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					discharged
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: '#ff7575'}}>
+					  <LocalHospital style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					hospitalised
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: '#ff0000'}}>
+					  <LocalHospital style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					critical
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+						<IconButton style={{ padding: '1px',backgroundColor: '#545454'}}>
+						  <Close style={{fontSize: 16}}/>
+						</IconButton>
+				  </td>
+                  <td>
+					deceased
+				  </td>
+                </tr>
+			</table>
+		)
+	}
+	else if (selectedIndex == 1){
+		const confirmedColour = '#1c7800'
+		const possiblyColour = '#545454'
+		return(
+			<table>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+					  <FlightLand style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					imported
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: confirmedColour}}>
+					  <People style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					Local Close Contact
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+					  <PersonPin style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					Possibly Local
+				  </td>
+                </tr>
+                <tr>
+                  <td>
+					<IconButton style={{ padding: '1px',backgroundColor: possiblyColour}}>
+					  <People style={{fontSize: 16}}/>
+					</IconButton>
+				  </td>
+                  <td>
+					Possibly Local Close Contact
+				  </td>
+                </tr>
+			</table>
+		)
+	}
+	return null
   }
   getClientDetail = (case_no)=>{
     var detail = null
@@ -164,9 +267,11 @@ class Map extends Component {
   }
   render() {
     const AnyReactComponent = this.AnyReactComponent
+	const {anchorEl, selectedIndex} = this.state
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div >
+		<div style={{ height: '85vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAd5lEr3jOvC2knuxINRDIznu6xWEVsfcw' }}
           defaultCenter={this.props.center}
@@ -176,6 +281,49 @@ class Map extends Component {
             this.List()
           }
         </GoogleMapReact>
+		</div>
+		
+		<Grid container style = {{flexGrow: 1}}spacing={2}>
+			<Grid item xs={2} style={{width: 100}}>
+				<List component="nav" aria-label="Device settings">
+					<ListItem
+					  button
+					  aria-haspopup="true"
+					  aria-controls="lock-menu"
+					  aria-label="when device is locked"
+					  onClick={this.handleClickListItem}
+					>
+						<ListItemText primary="Filter by:" secondary={options[selectedIndex]} />
+					</ListItem>
+				</List>
+				<Menu
+					id="lock-menu"
+					anchorEl={anchorEl}
+					keepMounted
+					open={Boolean(anchorEl)}
+					onClose={this.handleClose}
+					PaperProps={{
+					  style: {
+						maxHeight: 100,
+						width: '20ch',
+					  },
+					}}
+				>
+					{options.map((option, index) => (
+					  <MenuItem
+						key={option}
+						selected={index === selectedIndex}
+						onClick={(event) => this.handleMenuItemClick(event, index)}
+					  >
+						{option}
+					  </MenuItem>
+					))}
+				</Menu>
+			</Grid>
+			<Grid item xs={2}>
+			  <this.MapKey selectedIndex = {selectedIndex}/>
+			</Grid>
+		</Grid>
       </div>
     );
   }
