0fb82464739aa7b26294a3f3e62ef4cc1f2967c3
C.Victor <c>
2020-05-12 20:24:19 +0800

    [New] Add file include in timeblock

--- src/Component/TimeBlock/TimeBlock.js
+++ src/Component/TimeBlock/TimeBlock.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import CaseLocationJson from '../../Config/CaseLocation.json';
+import CasesDetailsJson from '../../Config/CasesDetails.json';
 class TimeBlock extends Component {
   
   render() {
